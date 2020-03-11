866104ec6d66737eaab363874632d472fde5dc82
Tobias Richling <RichlinT@apetito.com>
2020-03-11 17:03:22 +0100

    add handler

--- lunchero.Pricing/lunchero.Pricing.Application/Baskets/AddArticleToBasketHandler.cs
+++ lunchero.Pricing/lunchero.Pricing.Application/Baskets/AddArticleToBasketHandler.cs
@@ -1,16 +1,18 @@
 using System;
+using System.Threading.Tasks;
 using NServiceBus;
-using lunchero.Pricing.Contracts.Baskets;
+using NServiceBus.Logging;
+using lunchero.Pricing.Contracts.Baskets.Messages.Commands;
 
 namespace lunchero.Pricing.Application.Baskets
 {
     public class AddArticleToBasketHandler : IHandleMessages<AddArticleToBasket>
     {
-        public static ILog Log = LogManager.GetLogger<OrderPlacedHandler>();
+        public static ILog Log = LogManager.GetLogger<AddArticleToBasketHandler>();
 
         public async Task Handle(AddArticleToBasket message, IMessageHandlerContext context)
         {
-            Log.Info($"Adding article {message.ArticleId} to basket calculating prices");
+            Log.Info($"Adding article {message.ArticleNumber} to basket calculating prices");
             
             await Task.CompletedTask;
         }
--- lunchero.Pricing/lunchero.Pricing.Application/Baskets/CheckoutBasketHandler.cs
+++ lunchero.Pricing/lunchero.Pricing.Application/Baskets/CheckoutBasketHandler.cs
@@ -1,11 +1,20 @@
 using System;
+using System.Threading.Tasks;
 using NServiceBus;
-using lunchero.Pricing.Contracts.Baskets;
+using NServiceBus.Logging;
+using lunchero.Pricing.Contracts.Baskets.Messages.Commands;
 
 namespace lunchero.Pricing.Application.Baskets
 {
     public class CheckoutBasketHandler : IHandleMessages<CheckOutBasket>
     {
-        
+        public static ILog Log = LogManager.GetLogger<CheckoutBasketHandler>();
+
+        public async Task Handle(CheckOutBasket message, IMessageHandlerContext context)
+        {
+            Log.Info($"CheckoutBasketHandler");
+            
+            await Task.CompletedTask;
+        }
     }
 }
